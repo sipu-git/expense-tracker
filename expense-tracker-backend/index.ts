b0bb8562020fe0afad35f2d772ead2e8b32306fb
Sipu-982 <sipurana38@gmail.com>
2026-06-18 15:19:12 +0530

    server file modified

--- expense-tracker-backend/index.ts
+++ expense-tracker-backend/index.ts
@@ -5,14 +5,17 @@ import userRoutes from "./src/modules/user-service/user.routes.js";
 import groupRoutes from "./src/modules/group-service/group.routes.js";
 import expenseRoutes from "./src/modules/expense-service/expenses.routes.js";
 import authRoutes from './src/modules/auth-service/auth.routes.js';
-
 import cookiesParser from "cookie-parser";
+dotenv.config();
+
 const app = exxpress();
 app.use(exxpress.json());
 app.use(
     cors({
-        origin: ["http://localhost:5173","https://expense-tracker-steel-delta-33.vercel.app","https://expense-tracker-1-6m9p.onrender.com"],
+        origin: ["http://localhost:5173", "https://expense-tracker-steel-delta-33.vercel.app", "https://expense-tracker-1-6m9p.onrender.com"],
         credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 ); app.use(cookiesParser());
 app.use("/api/users", userRoutes);
@@ -20,7 +23,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/group", groupRoutes);
 app.use("/api/expenses", expenseRoutes);
 
-dotenv.config();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
