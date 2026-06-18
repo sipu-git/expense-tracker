ed46202be080f94552ba3f8d47f95b30a11de4d7
Sipu-982 <sipurana38@gmail.com>
2026-06-18 15:47:17 +0530

    BASE URL removed

--- expense-tracker-frontend/src/utils/api.ts
+++ expense-tracker-frontend/src/utils/api.ts
@@ -1,12 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://expense-tracker-1-6m9p.onrender.com/api";
-
-if (!BASE_URL) {
-    console.error("base url configuration issue!")
-}
-
 export const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: "/api",
     withCredentials: true
 })
