ccf85f1babc4750ff6141c3a4d6a7dc9e5e52c4d
Sipu-982 <sipurana38@gmail.com>
2026-06-18 16:04:55 +0530

    redis.ts file modified

--- expense-tracker-backend/src/shared/configs/redis.ts
+++ expense-tracker-backend/src/shared/configs/redis.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const redisUrl = process.env.REDIS_URL;
 if (!redisUrl) {
-    throw new Error("Redis url is not defined in .env")
+    throw new Error("Redis url isn't defined in .env")
 }
 const redis = createClient({
     url: redisUrl,
