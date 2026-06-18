aeaa3b4e56a1dea2549f32a291bd5b038603263c
Sipu-982 <sipurana38@gmail.com>
2026-06-18 13:32:32 +0530

    package.json file modified

--- expense-tracker-backend/index.ts
+++ expense-tracker-backend/index.ts
@@ -11,7 +11,7 @@ const app = exxpress();
 app.use(exxpress.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173","https://expense-tracker-steel-delta-33.vercel.app"],
         credentials: true,
     })
 ); app.use(cookiesParser());
