ed48d85b0810f27b42710e1758c3ab20566e172e
Sipu-982 <sipurana38@gmail.com>
2026-06-25 14:51:58 +0530

    file duplicacy removed

--- expense-tracker-frontend/src/utils/api.ts
+++ expense-tracker-frontend/src/utils/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { store } from "@/store";
 
-const BASE_URL="http://localhost:3000/api";
+// const BASE_URL="http://localhost:3000/api";
 export const api = axios.create({
-    // baseURL: "/api",
+    baseURL: "/api",
     baseURL: BASE_URL,
     withCredentials: true
 });
